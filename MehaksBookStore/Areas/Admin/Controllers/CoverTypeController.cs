dd5dd7ee88eea68646cc4e1b34cf9a8967898629
W0825072 <W0825072@myscc.ca>
2023-11-18 16:38:55 -0500

    Added js file and linked it to index in script to showcase the changes

--- MehaksBookStore/Areas/Admin/Controllers/CoverTypeController.cs
+++ MehaksBookStore/Areas/Admin/Controllers/CoverTypeController.cs
@@ -17,13 +17,68 @@ namespace MehaksBookStore.Areas.Admin.Controllers
         {
             return View();
         }
+        public IActionResult Upsert(int? id) //action method for upsert
+        {
+            CoverType covertype = new CoverType();//using NiharBooks.Models;
+            if (id == null)
+            {
+                // this is to creatte
+                return View(covertype);
+            }
+
+            //this for the edit
+            covertype = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
+            if (covertype == null)
+            {
+                return NotFound();
+            }
+            return View(covertype);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType covertype)
+        {
+            if (ModelState.IsValid)
+            {
+                if (covertype.Id == 0)
+                {
+                    _unitOfWork.CoverType.Add(covertype);
+
+                }
+                else
+                {
+                    _unitOfWork.CoverType.Update(covertype);
+                }
+                _unitOfWork.Save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(covertype);
+        }
+
+
+        //API Calls
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
         {
-            var allObj = _unitOfWork.Category.GetAll();
+            var allObj = _unitOfWork.CoverType.GetAll();
             return Json(new { data = allObj });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objFromDb = _unitOfWork.CoverType.Get(id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleteing" });
+            }
+            _unitOfWork.CoverType.Remove(objFromDb);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
         }
         #endregion
     }
+
 }
+
