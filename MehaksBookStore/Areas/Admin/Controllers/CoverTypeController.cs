247bbe4fc46ea594e96730136731b82697a7b52b
W0825072 <W0825072@myscc.ca>
2023-11-18 15:44:28 -0500

    Created CoverType Controller and a view to link it with. Followed by making changes in layoutpage to show the link to that index page.

---
+++ MehaksBookStore/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,29 @@
+﻿using MehaksBooks.DataAccess.Repository.IRepository;
+using MehaksBooks.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MehaksBookStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var allObj = _unitOfWork.Category.GetAll();
+            return Json(new { data = allObj });
+        }
+        #endregion
+    }
+}
